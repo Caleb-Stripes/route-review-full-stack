d01d47d77b2736f12ae4dc24a17ffc965aef1504
Caleb-Stripes <calebdstripes@gmail.com>
2020-02-29 15:58:29 -0500

    empty project

---
+++ src/main/java/org/wecancodeit/RouteReviewApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteReviewApplication.class, args);
+	}
+
+}
